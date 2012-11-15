ce867582535664eb326db3d75bf91076c7f40c8b
Mark Bates <mark@markbates.com>
2012-11-15 11:27:01 -0500

    added finder methods for sunspot

--- lib/sunspot/mongo.rb
+++ lib/sunspot/mongo.rb
@@ -6,8 +6,19 @@ module Sunspot
     def self.included(base)
       base.class_eval do
         extend Sunspot::Rails::Searchable::ActsAsMethods
+        extend Sunspot::Mongo::Finders
         Sunspot::Adapters::DataAccessor.register(DataAccessor, base)
         Sunspot::Adapters::InstanceAdapter.register(InstanceAdapter, base)
+      end
+    end
+
+    module Finders
+      def sunspot_find(id)
+        find(id)
+      end
+
+      def sunspot_find_all(ids)
+        find(ids)
       end
     end
 
@@ -19,11 +30,11 @@ module Sunspot
 
     class DataAccessor < Sunspot::Adapters::DataAccessor
       def load(id)
-        @clazz.find(id)
+        @clazz.sunspot_find(id)
       end
 
       def load_all(ids)
-        @clazz.find(ids)
+        @clazz.sunspot_find_all(ids)
       end
     end
   end
